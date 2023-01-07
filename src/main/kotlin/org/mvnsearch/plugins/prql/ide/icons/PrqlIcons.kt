c78d32e18e5d99e2279eac920014c05469d79858
Libing Chen <libing.chen@gmail.com>
2023-01-07 14:08:28 +0800

    chore: add COPY_ICON

--- src/main/kotlin/org/mvnsearch/plugins/prql/ide/icons/PrqlIcons.kt
+++ src/main/kotlin/org/mvnsearch/plugins/prql/ide/icons/PrqlIcons.kt
@@ -1,9 +1,8 @@
 package org.mvnsearch.plugins.prql.ide.icons
 
-import com.intellij.icons.AllIcons
 import com.intellij.openapi.util.IconLoader
 
 object PrqlIcons {
     val PRQL_FILE = IconLoader.findIcon("/icons/prql-16x16.png", PrqlIcons::class.java.classLoader)!!
-    val RUN_ICON = AllIcons.RunConfigurations.TestState.Run
+    val COPY_ICON = com.intellij.icons.AllIcons.Actions.Copy
 }
