1348b3f42fd8670d2362a31523deede0386163d0
Libing Chen <libing.chen@gmail.com>
2023-01-07 14:10:33 +0800

    app: Prql

---
+++ src/main/kotlin/org/mvnsearch/plugins/prql/Prql.kt
@@ -0,0 +1,22 @@
+package org.mvnsearch.plugins.prql
+
+import com.intellij.openapi.util.SystemInfo
+import java.io.File
+
+object Prql {
+    fun getPrqlCompilerCmdAbsolutionPath(): String {
+        val userHome = System.getProperty("user.home")
+        if (File(userHome, ".cargo/bin/prql-compiler").exists()) {
+            return File(userHome, ".cargo/bin/prql-compiler").absolutePath
+        }
+        return if (SystemInfo.isWindows) {
+            return "just"
+        } else {
+            if (File("/usr/local/bin/prql-compiler").exists()) {
+                "/usr/local/bin/prql-compiler"
+            } else {
+                "prql-compiler"
+            }
+        }
+    }
+}
