60cf70d1137de5a74a7b6b3d98bf904712315482
Libing Chen <libing.chen@gmail.com>
2023-01-09 13:26:48 +0800

    chore: use version to download intellij

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,8 +37,8 @@ kotlin {
 intellij {
     pluginName.set(properties("pluginName"))
     type.set(properties("platformType"))
-    //version.set(properties("platformVersion"))
-    localPath.set(System.getProperty("user.home") + "/.gradle/caches/modules-2/files-2.1/com.jetbrains.intellij.idea/ideaIU/2022.3/56e5440ce30b03c913ff231a46cb1ba6ac4df63/ideaIU-2022.3")
+    version.set(properties("platformVersion"))
+    //localPath.set(System.getProperty("user.home") + "/.gradle/caches/modules-2/files-2.1/com.jetbrains.intellij.idea/ideaIU/2022.3/56e5440ce30b03c913ff231a46cb1ba6ac4df63/ideaIU-2022.3")
 
     // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
     plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
