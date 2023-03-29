709f0807636bf588d5e1bafdbc621b1b88230582
Libing Chen <libing.chen@gmail.com>
2023-03-29 19:22:16 +0800

    feat: add transformPrql

--- src/main/kotlin/org/mvnsearch/plugins/prql/Prql.kt
+++ src/main/kotlin/org/mvnsearch/plugins/prql/Prql.kt
@@ -1,5 +1,9 @@
 package org.mvnsearch.plugins.prql
 
+import com.intellij.openapi.project.Project
+import com.intellij.sql.dialects.SqlDialectMappings
+import org.prql.prql4j.PrqlCompiler
+
 object Prql {
     private val dialectMapping = mapOf(
         "PostgreSQL" to "postgres",
@@ -14,7 +18,23 @@ object Prql {
         "Snowflake" to "snowflake",
     )
 
-    fun getTarget(dialectId: String): String? {
-        return dialectMapping[dialectId]
+    @Throws(Exception::class)
+    fun transformPrql(dialect: String?, prqlCode: String, format: Boolean, project: Project): String {
+        val prqlTarget = if (dialect != null) {
+            dialectMapping[dialect]
+        } else {
+            val defaultDialect = SqlDialectMappings.getMapping(project, null)
+            dialectMapping[defaultDialect.id]
+        } ?: "generic"
+        val sql = if (prqlCode.contains(" ?")) {
+            PrqlCompiler.toSql(prqlCode.replace(" ?", "$0"), "sql.${prqlTarget}", format, false)
+        } else {
+            PrqlCompiler.toSql(prqlCode, "sql.${prqlTarget}", format, false)
+        }
+        return if (sql.contains(" $0")) {
+            sql.replace(" $0", " ?")
+        } else {
+            sql
+        }
     }
 }
