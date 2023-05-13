6af15e5193abc7c595e00b9c6828a668b5ba450b
Libing Chen <libing.chen@gmail.com>
2023-05-13 10:36:48 +0800

    build: update to prql-java-0.8.2.jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ repositories {
 }
 
 dependencies {
-    implementation(files("libs/prql-java-0.8.0.jar"))
+    implementation(files("libs/prql-java-0.8.2.jar"))
 }
 
 // Set the JVM language level used to build project. Use Java 11 for 2020.3+, and Java 17 for 2022.2+.
