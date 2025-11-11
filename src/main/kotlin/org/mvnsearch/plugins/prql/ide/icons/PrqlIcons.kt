f7b2d3e07d37edc12058677f48e98441f44b00b0
Liging Chen <libing.chen@gmail.com>
2025-11-11 13:25:46 +0800

    chore: add TO_SQL_ICON

--- src/main/kotlin/org/mvnsearch/plugins/prql/ide/icons/PrqlIcons.kt
+++ src/main/kotlin/org/mvnsearch/plugins/prql/ide/icons/PrqlIcons.kt
@@ -5,4 +5,5 @@ import com.intellij.openapi.util.IconLoader
 object PrqlIcons {
     val PRQL_FILE = IconLoader.findIcon("/icons/prql-16x16.png", PrqlIcons::class.java.classLoader)!!
     val COPY_ICON = com.intellij.icons.AllIcons.Actions.Copy
+    val TO_SQL_ICON = IconLoader.findIcon("/icons/tosql.svg", PrqlIcons::class.java.classLoader)!!
 }
